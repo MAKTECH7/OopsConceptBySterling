55fb04dd1474fab526891f0831adfe96fdd5426c
MAK TECH <105592640+MAKTECH7@users.noreply.github.com>
2024-11-26 02:09:57 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,29 @@
+ class Shape{
+//private int borderWidth =0;
+//private String Color = null;
+
+
+
+  public Shape(){
+  System.out.println("this is no parameterized constructor");
+ }
+
+
+
+
+/*
+public Shape(int borderWidth,String Color){
+this.borderWidth = borderWidth;
+this.Color= Color;
+}
+public int getBorderWidth(){
+return borderWidth;
+}
+public String getColor(){
+return Color;
+}
+*/
+
+
+
+}
