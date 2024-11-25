1bdfe8d648627c285e4416229834fd06ef4d4240
MAK TECH <105592640+MAKTECH7@users.noreply.github.com>
2024-11-26 02:10:43 +0530

    Create TestConstructor.java

---
+++ TestConstructor.java
@@ -0,0 +1,14 @@
+ class TestShape{
+
+ public static void main(String[] args){
+
+Shape S = new Shape();
+
+/*
+Shape S1 = new Shape(10, "MAK");
+  
+System.out.println(S1.getBorderWidth());
+System.out.println(S1.getColor());
+*/
+  }
+}
