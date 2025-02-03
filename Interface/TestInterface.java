d149bd0b7072c89df1049de421b30538e7a960c1
MAK TECH <105592640+MAKTECH7@users.noreply.github.com>
2025-02-03 23:53:53 +0530

    Add files via upload

---
+++ Interface/TestInterface.java
@@ -0,0 +1,25 @@
+public interface TestInterface {
+public static void main(String[] args){
+
+System.out.println("Hello from jdk 1.8 Interface");
+
+
+}
+
+public default void add(){
+
+//default keyword for body
+
+}
+
+private void sub(){
+
+
+}
+
+private static void div(){
+
+}
+
+
+}
